35c0fc8b6bddeb17b6a122c9996cb03678c07b78
Link4Electronics <link4electronics@gmail.com>
2026-04-21 02:54:26 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -16,6 +16,11 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+echo "Getting app..."
+echo "---------------------------------------------------------------"
 wget https://projectmagma.net/downloads/myth2_updates/1.8.5%20Final%20(Build%20471b)/Myth2_185_Linux.tar.gz
+tar -xvf Myth2_185_Linux.tar.gz
+rm -f *.gz *.pdf *.txt Myth2_32bit .DS_Store
 
 mkdir -p ./AppDir/bin
+mv -v plugins Myth2_64bit ./AppDir/bin
