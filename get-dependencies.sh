fdd5cf5ef42cbf1b6fa3a8202daf52345cb1a57c
Link4Electronics <link4electronics@gmail.com>
2026-04-21 02:32:55 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -17,3 +17,5 @@ get-debloated-pkgs --add-common --prefer-nano
 
 # If the application needs to be manually built that has to be done down here
 wget https://projectmagma.net/downloads/myth2_updates/1.8.5%20Final%20(Build%20471b)/Myth2_185_Linux.tar.gz
+
+mkdir -p ./AppDir/bin
